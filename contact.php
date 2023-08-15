e2e0ce20911951dbff3e418f82db908a9a3a36f7
BHANU PRATAP BISWAS <63925690+BS-World@users.noreply.github.com>
2023-08-15 21:22:09 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'bhanubiswas98@gmail.com';
+  $receiving_email_address = 'bhanubiswas97@gmail.com';
 
   if( file_exists($php_email_form = 'php-email-form.php' )) {
     include( $php_email_form );
